f22e634d88dbeae817ccc07794fd714a55ec19f3
Faydiamond <fabiansochabarbon@gmail.com>
2018-06-01 11:01:34 -0500

    was created in vs2013

---
+++ Tabledata/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tabledata.Models;
+
+namespace Tabledata.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MostData()
+        {
+            using (MyDataBaseEntities Bd=new MyDataBaseEntities())
+            {
+                var Datos = Bd.pacientes.OrderBy(X => X.Nombre).ToList();
+                return Json(new { data = Datos }, JsonRequestBehavior.AllowGet);
+            }
+        }
+	}
+}
---
+++ Tabledata/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tabledata.MvcApplication" Language="C#" %>
